1ba1538e3149da59500b40bbcc4945c4b575b293
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2023-10-06 10:34:46 -0300

    Api maven

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# API com spring boot 
+
+Projeto feito como desafio de projeto para o Bootcamp Santander DIO Java.
---
+++ src/main/java/zanlucas/laiz/bootcamp/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package zanlucas.laiz.bootcamp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import zanlucas.laiz.bootcamp.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+}
